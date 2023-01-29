3dbcd5958eeeb7efcf98b244c6e4aa55a1c7efee
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2023-01-29 12:19:17 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+echo "正在关闭并卸载vlmcsd. . ."
+
+systemctl stop vlmcsd-x64-musl
+
+systemctl disable vlmcsd-x64-musl
+
+rm -rf /opt/binaries
+
 echo "正在下载vlmcsd. . ."
 
 wget https://github.com/Wind4/vlmcsd/releases/download/svn1113/binaries.tar.gz -O /opt/binaries.tar.gz
